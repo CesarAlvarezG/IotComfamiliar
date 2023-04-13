7b7842be37bc6bfd9faf8a096491a29025ca9ea9
Cesar Augusto Alvarez Gaspar <ing.cesaralvarezg@gmail.com>
2023-04-13 11:19:32 -0500

    Inculsion del sensor de temperatura y humedad

--- IotComfamiliar.ino
+++ IotComfamiliar.ino
@@ -1,16 +1,24 @@
 #include <Wire.h>
 #include "SSD1306Wire.h"
+#include "DHT.h"
 
+#define DTHPIN 18
+#define DTHTYPE DHT11
+
+DHT dht(DTHPIN, DTHTYPE);
 SSD1306Wire display(0x3c, SDA, SCL);
 
 const String Bienvenida="Sistema de medición de calidad del Aire";
-
+String Consola="";
+float h=0;//Humedad
+float t=0;//Temperatura
 
 void setup() {
    Serial.begin(115200);
    Serial.println("Sistema de medición de calidad del Aire");
    Serial.println("Comfamiliar Pereira");
    Serial.println("César Augusto Álvarez Gaspar");
+   dht.begin();
    display.init();
    display.setContrast(255);
    display.setTextAlignment(TEXT_ALIGN_LEFT);
@@ -22,9 +30,18 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+ h=dht.readHumidity();
+ t=dht.readTemperature();
+ Serial.print(h);
+ Serial.print(" ");
+ Serial.println(t);
+ Consola="";
+ Consola+=h;
+ Consola+=" ";
+ Consola+=t;  
  static uint16_t start_at = 0;
   display.clear();
-  uint16_t firstline = display.drawStringMaxWidth(0, 0, 128, Bienvenida.substring(start_at));
+  uint16_t firstline = display.drawStringMaxWidth(0, 0, 128, Consola.substring(start_at));
   display.display();
   if (firstline != 0) {
     start_at += firstline;
